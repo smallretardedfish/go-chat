ba9a6bf8444a61ded0ca6a326a63f948b516ad11
smallretardedfish <hitriy.koliy@gmail.com>
2022-05-24 20:01:31 +0300

    imlemented few user repo methods

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/smallretardedfish/go-chat/configs"
-	"github.com/smallretardedfish/go-chat/repositories/user_repo"
+	"github.com/smallretardedfish/go-chat/internal/repositories/user_repo"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 )
+
+func NewDb(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return db, nil
+}
 
 func main() {
 	cfg, err := configs.NewConfig()
@@ -12,12 +24,31 @@ func main() {
 		log.Fatal(err)
 	}
 
-	db, err := .NewDb(cfg.DSN)
+	db, err := NewDb(cfg.DSN)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	userRepo := user_repo.NewUserRepo(db)
+	if err := db.AutoMigrate(user_repo.User{}); err != nil {
+		log.Println(err)
+		return
+	}
+	//db.AutoMigrate(message_repo.Message{})
+	//db.AutoMigrate(room_repo.Room{})
 
+	//u := user_repo.User{
+	//	Name:      "John",
+	//	CreatedAt: time.Now(),
+	//}
+
+	//user, err := userRepo.CreateUser(u)
+	user, err := userRepo.GetUser(1)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(user.UpdatedAt)
 	// Tests
 }
